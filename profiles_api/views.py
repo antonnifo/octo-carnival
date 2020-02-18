01c9953ded15785d0696e98f1a6ce3a330f9144e
antonnifo <antonnifo@live.com>
2020-02-18 11:48:01 +0300

    add search filter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.response import Response
 from rest_framework.views import status
 from rest_framework.authentication import TokenAuthentication
@@ -14,3 +14,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset         = UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes     = (UpdateOwnProfile,)
+    filter_backends  = (filters.SearchFilter,)
+    search_fields    = ('name', 'email',)  
