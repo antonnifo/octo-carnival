c2528f2d507bcfe0950022dce7977d52034eb47f
antonnifo <antonnifo@live.com>
2020-02-18 10:48:18 +0300

    create Serializer for a user profilr object

---
+++ profiles_api/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+
+from .models import UserProfile
+
+
+# class HelloSerializer(serializers.Serializer):
+#     """Seriazers and name field for testing APIView"""
+
+#     name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializer for a user profilr object"""
+
+    class Meta:
+        model  = UserProfile
+        fields = ('id','name','email','password')
+        extra_kwargs = {
+            'password':{
+                'write_only':True,
+                'style':{
+                    'input_type':'password'
+                }
+            }
+        } 
+
+    def create(self,validated_data):
+        """create and return new user"""
+
+        user = UserProfile.objects.create_user(
+            email    = validated_data['email'],
+            name     = validated_data['name'],
+            password = validated_data['password']
+        )    
+        return user
