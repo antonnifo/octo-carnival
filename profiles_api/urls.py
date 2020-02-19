b6e959d2be5fe513278a1f255c3b222a345ccff2
antonnifo <antonnifo@live.com>
2020-02-19 06:52:15 +0300

    add path for login

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import UserProfileViewSet
+from .views import UserProfileViewSet, UserLoginApiView
 
 router = DefaultRouter()
 router.register('profile', UserProfileViewSet)
 
 urlpatterns = [
-     path('', include(router.urls))
+     path('', include(router.urls)),
+     path('login/', UserLoginApiView.as_view()),
 ]
