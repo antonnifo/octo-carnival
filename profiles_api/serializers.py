f7b1c5b86bbcb25d0ad232da797b25e0eb70a87b
antonnifo <antonnifo@live.com>
2020-02-20 09:53:41 +0300

    serializer profile feed items

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import UserProfile
+from .models import UserProfile, ProfileFeedItem
 
 
 # class HelloSerializer(serializers.Serializer):
@@ -32,3 +32,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
             password = validated_data['password']
         )    
         return user
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """serializer profile feed items"""
+
+    class Meta:
+        model  = ProfileFeedItem
+        fields = ('id','user_profile','status_text','created_on')
+
+        extra_kwargs = {'user_profile': {'read_only': True}} 
