00334c54268d7d43c7adc60b6f05cd30ff051756
antonnifo <antonnifo@live.com>
2020-02-20 09:51:57 +0300

    register feeds viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import UserProfileViewSet, UserLoginApiView
+from .views import UserProfileViewSet, UserLoginApiView, UserProfileFeedViewSet
 
 router = DefaultRouter()
 router.register('profile', UserProfileViewSet)
+router.register('feed', UserProfileFeedViewSet)
 
 urlpatterns = [
      path('', include(router.urls)),
